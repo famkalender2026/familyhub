38bc5836472a977a30529ca8abb4693b53b9c4bd
famkalender2026 <ai.assistence.app.generator@gmail.com>
2026-03-28 14:59:05 +0100

    Initial commit: FamilyHub App mit Service Worker

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+// Service Worker für FamilyHub
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
